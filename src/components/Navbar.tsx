9133cf6662b95bf75c4f5383e2912c3ec18141e5
WhatWR <kul.b@ku.th>
2023-01-16 22:49:32 +0700

    logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,26 @@
-import React from "react";
-import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
-import { TiMicrophone } from "react-icons/ti";
-import { BsYoutube, BsCameraVideo, BsBell } from "react-icons/bs";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { IoAppsSharp } from "react-icons/io5";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../store/hooks";
-import { changeSearchTerm, clearSearchTerm, clearVideos } from "../store";
-import { getSearchPageVideos } from "../store/reducers/getSearchPageVideos";
+import React from 'react'
+import { AiOutlineSearch, AiOutlineClose } from 'react-icons/ai'
+import { TiMicrophone } from 'react-icons/ti'
+import { BsYoutube, BsCameraVideo, BsBell } from 'react-icons/bs'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { IoAppsSharp } from 'react-icons/io5'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { useAppDispatch, useAppSelector } from '../store/hooks'
+import { changeSearchTerm, clearSearchTerm, clearVideos } from '../store'
+import { getSearchPageVideos } from '../store/reducers/getSearchPageVideos'
 
 export default function Navbar() {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
+  const location = useLocation()
+  const navigate = useNavigate()
+  const dispatch = useAppDispatch()
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm)
   const handleSearch = () => {
-    if (location.pathname !== "/search") navigate("/search");
+    if (location.pathname !== '/search') navigate('/search')
     else {
-      dispatch(clearVideos());
-      dispatch(getSearchPageVideos(false));
+      dispatch(clearVideos())
+      dispatch(getSearchPageVideos(false))
     }
-  };
+  }
 
   return (
     <div className="flex justify-between items-center  px-14 h-14 bg-[#212121] opacity-95 sticky top-0 z-50">
@@ -38,8 +38,8 @@ export default function Navbar() {
       <div className="flex items-center justify-center gap-5">
         <form
           onSubmit={(e) => {
-            e.preventDefault();
-            handleSearch();
+            e.preventDefault()
+            handleSearch()
           }}
         >
           <div className="flex bg-zinc-900 items-center h-10 px-4 pr-0">
@@ -56,7 +56,7 @@ export default function Navbar() {
 
               <AiOutlineClose
                 className={`text-xl cursor-pointer ${
-                  !searchTerm ? "invisible" : "visible"
+                  !searchTerm ? 'invisible' : 'visible'
                 }`}
                 onClick={() => dispatch(clearSearchTerm())}
               />
@@ -80,11 +80,11 @@ export default function Navbar() {
           </span>
         </div>
         <img
-          src="https://yt3.ggpht.com/wgnEbc2Ec2JYkeyzUbiHzDlAFObI2Btwo2YRCEF1aCMBiRc5E_zWy8-URBQS3EMQ1yzzaGFR=s88-c-k-c0x00ffffff-no-rj-mo"
+          src="https://www.iconbolt.com/preview/twitter/radix-icons/vercel-logo.svg"
           className="w-9 h-9 rounded-full"
           alt="logo"
         />
       </div>
     </div>
-  );
+  )
 }
